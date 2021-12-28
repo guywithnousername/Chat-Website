0cdc453f71504ca215c9f435ac0a307dc5cb4904
Minglang Du <mldu@cydu.net>
2021-12-27 19:58:53 -0800

    add chat blueprint

---
+++ chat.py
@@ -0,0 +1,56 @@
+from flask import *
+from flask import render_template as rend
+import database
+import sqlite3
+from cryptography.fernet import Fernet
+from datetime import datetime
+
+chatpage = Blueprint('chatpage',__name__,template_folder="templates",static_folder="static")
+cryptokey = b'RwdEWFPygOggOdXRkNSKGM8Wm58QT6ZIpZ34oauwkSE='
+fernet = Fernet(cryptokey)
+
+@chatpage.route("/chatroom/<room>",methods = ['GET','POST'])
+def chat(room):
+    con = database.get_db()
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    encryptedroomname = bytes(request.cookies.get("Room"),'utf-8')
+    decodedroom = fernet.decrypt(encryptedroomname).decode()
+    if (room != decodedroom):
+        return rend("message.html",message="You cannot access this room.")
+    if request.method == "POST":
+        message = request.form.get("message")
+        name = request.cookies.get("Username") or 'Unnamed'
+        time = datetime.now().strftime("%B %d, %Y %I:%M%p")
+        acttime = int(datetime.now().strftime("%s"))
+        cur.execute("""
+        INSERT INTO Messages (RoomName,MSG,Username,Timestring,Time)
+        VALUES (?,?,?,?,?)
+        """, (room.title(),message,name,time,acttime))
+        con.commit()
+    select = database.query_db("""
+        SELECT MSG,Username,Timestring FROM Messages
+        WHERE RoomName = ?
+        ORDER BY Time DESC
+        """,(room.title(),))
+    con.close()
+    select = [(x[0],x[1],x[2]) for x in select]
+    return rend("room.html",messages=select)
+
+@chatpage.route("/roomselect",methods=['GET','POST'])
+def selrooms():
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        con = database.get_db()
+        cur = con.cursor()
+        cur = cur.execute("SELECT Pass FROM Rooms WHERE RName = ? ", (name.title(), ))
+        ret = cur.fetchone()
+        if not ret:
+            return rend("message.html",message=f"The room {name.title()} was not found.")
+        if ret['Pass'] != password: 
+            return rend("message.html",message="The password was wrong.")
+        resp = make_response(redirect(f"/chatroom/{(name.title())}",code=302))
+        resp.set_cookie('Room',fernet.encrypt(name.title().encode()))
+        return resp
+    return rend("usernameform.html",type="Join a room")
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from datetime import datetime
 import sqlite3
 from database import *
 from cryptography.fernet import Fernet
+from chat import chatpage
 
 app = Flask(__name__)
+app.register_blueprint(chatpage)
 DATABASE = "database.db"
 app.secret_key = '9ac7d06219fbfa373f76c9a6be47b178157e2a91436b263b703c63246e25'
 cryptokey = b'RwdEWFPygOggOdXRkNSKGM8Wm58QT6ZIpZ34oauwkSE='
@@ -14,11 +16,11 @@ fernet = Fernet(cryptokey)
 @app.route("/")
 def index():
     name = request.cookies.get("Username")
-    password = query_db("SELECT * FROM Users WHERE Username = ?",args=(name,),one=True)["Pass"]
+    password = query_db("SELECT * FROM Users WHERE Username = ?",args=(name,),one=True)
     if request.cookies.get("Username") == None:
         return rend("index.html")
     else:
-        return rend("user.html",name=name,password=password)
+        return rend("user.html",name=name,password=password["Pass"])
 
 @app.route("/register",methods = ['GET','POST'])
 def reg():
@@ -66,53 +68,6 @@ def login():
             else:
                 return rend("message.html",message="You have entered the password incorrectly.")
     return rend('usernameform.html',type="Login to your account")
-
-@app.route("/chatroom/<room>",methods = ['GET','POST'])
-def chat(room):
-    init_db()
-    con = get_db()
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    encryptedroomname = bytes(request.cookies.get("Room"),'utf-8')
-    decodedroom = fernet.decrypt(encryptedroomname).decode()
-    if (room != decodedroom):
-        return rend("message.html",message="You cannot access this room.")
-    if request.method == "POST":
-        message = request.form.get("message")
-        name = request.cookies.get("Username") or 'Unnamed'
-        time = datetime.now().strftime("%B %d, %Y %I:%M%p")
-        acttime = int(datetime.now().strftime("%s"))
-        cur.execute("""
-        INSERT INTO Messages (RoomName,MSG,Username,Timestring,Time)
-        VALUES (?,?,?,?,?)
-        """, (room.title(),message,name,time,acttime))
-        con.commit()
-    select = query_db("""
-        SELECT MSG,Username,Timestring FROM Messages
-        WHERE RoomName = ?
-        ORDER BY Time DESC
-        """,(room.title(),))
-    con.close()
-    select = [(x[0],x[1],x[2]) for x in select]
-    return rend("room.html",messages=select)
-
-@app.route("/roomselect",methods=['GET','POST'])
-def selrooms():
-    if request.method == "POST":
-        name = request.form.get("name")
-        password = request.form.get("password")
-        con = get_db()
-        cur = con.cursor()
-        cur = cur.execute("SELECT Pass FROM Rooms WHERE RName = ? ", (name.title(), ))
-        ret = cur.fetchone()
-        if not ret:
-            return rend("message.html",message=f"The room {name.title()} was not found.")
-        if ret['Pass'] != password: 
-            return rend("message.html",message="The password was wrong.")
-        resp = make_response(redirect(f"/chatroom/{(name.title())}",code=302))
-        resp.set_cookie('Room',fernet.encrypt(name.title().encode()))
-        return resp
-    return rend("usernameform.html",type="Join a room")
 
 @app.route("/logout",methods = ['GET','POST'])
 def logout():

b615c49e1959d6c9d5726d50ba13a2a49b6a4b1e
Minglang Du <minglangdu@gmail.com>
2022-03-19 14:58:56 -0500

    Add player. (doesn't collide)

--- static/game.js
+++ static/game.js
@@ -23,7 +23,10 @@ function preload() {
 	this.load.image('soil','/static/assets/tilesets/plowed_soil.png');
 	this.load.image('fences','/static/assets/tilesets/fence.png');
 	this.load.image('sand','/static/assets/tilesets/sand.png');
+
 	this.load.tilemapTiledJSON('map', '/static/assets/farm.json');
+
+	this.load.image('cube','/static/assets/player.png');
 }
 function create() {
 	map = this.make.tilemap({ key: 'map' });
@@ -35,8 +38,22 @@ function create() {
 	let fences = map.addTilesetImage('fences','fences');
 	let fencelayer = map.createLayer('Fences',fences,0,0);
 
-	map.setCollisionByExclusion(-1,true,fencelayer);
+	map.setCollision(fencelayer);
+	this.cube = this.physics.add.sprite(240,240,"cube");
+
+	this.cursors = this.input.keyboard.createCursorKeys();
 }
 function update() {
-
+	if (this.cursors.left.isDown) {
+		this.cube.body.velocity.x -= 1;
+	} 
+	if (this.cursors.right.isDown) {
+		this.cube.body.velocity.x += 1;
+	}
+	if (this.cursors.up.isDown) {
+		this.cube.body.velocity.y -= 1;
+	}
+	if (this.cursors.down.isDown) {
+		this.cube.body.velocity.y += 1;
+	}
 }
